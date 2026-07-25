b146fd5feaa99d1c397333fa34eb27fcaf2d3d91
Omar Khaled <154836002+omar2004khaled@users.noreply.github.com>
2026-07-25 12:00:59 +0300

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        // Step 1: Find the middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse the second half
+        ListNode second = slow.next;
+        slow.next = null;
+
+        ListNode prev = null;
+        while (second != null) {
+            ListNode next = second.next;
+            second.next = prev;
+            prev = second;
+            second = next;
+        }
+
+        // Step 3: Merge the two halves
+        ListNode first = head;
+        second = prev;
+
+        while (second != null) {
+            ListNode next1 = first.next;
+            ListNode next2 = second.next;
+
+            first.next = second;
+            second.next = next1;
+
+            first = next1;
+            second = next2;
+        }
+    }
+}
