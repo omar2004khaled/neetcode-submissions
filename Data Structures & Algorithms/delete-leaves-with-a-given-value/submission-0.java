b759f5a60a4269969eff8ab14bd4c9ba750fcb37
Omar Khaled <154836002+omar2004khaled@users.noreply.github.com>
2026-08-27 16:01:20 +0300

    Add: delete-leaves-with-a-given-value - submission-0

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        
+        if (root == null) {
+            return null;
+        }
+
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        // Now check if root is a leaf
+        if (root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+
+        return root;
+    }
+}
